f730ac4b94e113c5d0496d93963f6c380d8785b0
Robert Marriott <13823574+robertmarriott@users.noreply.github.com>
2024-08-30 11:55:34 -0700

    Refactor for concision

--- src/CourseCatalogService/CourseCatalog.Domain/Common/ValueObject.cs
+++ src/CourseCatalogService/CourseCatalog.Domain/Common/ValueObject.cs
@@ -1,4 +1,4 @@
-﻿namespace CourseCatalog.Domain.Common;
+namespace CourseCatalog.Domain.Common;
 
 public abstract class ValueObject
 {
@@ -6,39 +6,23 @@ public abstract class ValueObject
 
     public override int GetHashCode()
     {
-        // HashMultiplier is a prime number that helps reduce hash collisions
         const int HashMultiplier = 31;
 
-        return GetEqualityComponents().Aggregate(0, (hashCode, value) =>
-            hashCode = hashCode * HashMultiplier ^ (value?.GetHashCode() ?? 0));
+        return GetEqualityComponents()
+            .Aggregate(0, (hash, value) =>
+                hash * HashMultiplier ^ (value?.GetHashCode() ?? 0));
     }
 
     public override bool Equals(object? obj)
     {
-        if (obj is null || obj.GetType() != GetType())
-        {
-            return false;
-        }
-
-        var other = (ValueObject)obj;
-
-        return GetEqualityComponents().SequenceEqual(
-            other.GetEqualityComponents());
+        return obj is ValueObject other
+            && GetEqualityComponents().SequenceEqual(
+                other.GetEqualityComponents());
     }
 
     public static bool operator ==(ValueObject left, ValueObject right)
     {
-        if (left is null && right is null)
-        {
-            return true;
-        }
-
-        if (left is null || right is null)
-        {
-            return false;
-        }
-
-        return left == right;
+        return left is null ? right is null : left.Equals(right);
     }
 
     public static bool operator !=(ValueObject left, ValueObject right)
