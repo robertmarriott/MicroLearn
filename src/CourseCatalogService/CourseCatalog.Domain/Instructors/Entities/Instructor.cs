5384915a53cf79550f35b2b6bdfdbb6ecf2b19d0
Robert Marriott <13823574+robertmarriott@users.noreply.github.com>
2024-09-02 16:36:23 -0700

    Add properties and static factory method

--- src/CourseCatalogService/CourseCatalog.Domain/Instructors/Entities/Instructor.cs
+++ src/CourseCatalogService/CourseCatalog.Domain/Instructors/Entities/Instructor.cs
@@ -1,7 +1,29 @@
 ﻿using CourseCatalog.Domain.Common;
+using CourseCatalog.Domain.Instructors.ValueObjects;
 
 namespace CourseCatalog.Domain.Instructors.Entities;
 
-public class Instructor : Entity, IAggregateRoot
+public class Instructor : AggregateRoot<InstructorId>
 {
+    public string FirstName { get; }
+    public string LastName { get; }
+    public string FullName => $"{FirstName} {LastName}";
+
+    private Instructor(InstructorId id, string firstName, string lastName)
+        : base(id)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+
+    public static Instructor Create(string firstName, string lastName)
+    {
+        ArgumentNullException.ThrowIfNullOrWhiteSpace(
+            firstName, nameof(firstName));
+
+        ArgumentNullException.ThrowIfNullOrWhiteSpace(
+            lastName, nameof(lastName));
+
+        return new Instructor(new InstructorId(), firstName, lastName);
+    }
 }
