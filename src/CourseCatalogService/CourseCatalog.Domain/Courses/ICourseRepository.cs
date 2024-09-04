ab469476503983e8998b4f4eee26ce0ba2fff675
Robert Marriott <13823574+robertmarriott@users.noreply.github.com>
2024-09-03 23:44:40 -0700

    Create repository interface

---
+++ src/CourseCatalogService/CourseCatalog.Domain/Courses/ICourseRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace CourseCatalog.Domain.Courses;
+
+public interface ICourseRepository
+{
+    Task<IEnumerable<Course>> GetAllAsync(int pageNumber, int pageSize);
+    Task<Course?> GetByIdAsync(CourseId id);
+    Task AddAsync(Course course);
+    Task UpdateAsync(Course course);
+    Task DeleteAsync(CourseId id);
+
+}
