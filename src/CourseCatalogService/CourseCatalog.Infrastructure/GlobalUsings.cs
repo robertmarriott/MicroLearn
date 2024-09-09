bc9bd93f55f9640bc519d5fb17fd50a358b425a2
Robert Marriott <13823574+robertmarriott@users.noreply.github.com>
2024-09-08 21:50:07 -0700

    Create initial UnitOfWork class

---
+++ src/CourseCatalogService/CourseCatalog.Infrastructure/GlobalUsings.cs
@@ -0,0 +1,1 @@
+﻿global using CourseCatalog.Domain.Interfaces;
---
+++ src/CourseCatalogService/CourseCatalog.Infrastructure/Persistence/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿namespace CourseCatalog.Infrastructure.Persistence;
+
+public class UnitOfWork : IUnitOfWork
+{
+    public void BeginTransaction()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task CommitTransactionAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task RollbackTransactionAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<int> SaveChangesAsync(
+        CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
