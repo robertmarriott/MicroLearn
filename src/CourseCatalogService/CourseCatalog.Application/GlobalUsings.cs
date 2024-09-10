ab7eb8bfdaca36a92c499b707db04d7b3d19a75f
Robert Marriott <13823574+robertmarriott@users.noreply.github.com>
2024-09-09 23:02:48 -0700

    Create course mapping profile

--- src/CourseCatalogService/CourseCatalog.Application/GlobalUsings.cs
+++ src/CourseCatalogService/CourseCatalog.Application/GlobalUsings.cs
@@ -1,7 +1,10 @@
-﻿global using CourseCatalog.Application.Exceptions;
+﻿global using AutoMapper;
+global using CourseCatalog.Application.Dtos;
+global using CourseCatalog.Application.Exceptions;
 global using CourseCatalog.Domain.Entities;
 global using CourseCatalog.Domain.Enums;
 global using CourseCatalog.Domain.Interfaces;
 global using CourseCatalog.Domain.Repositories;
 global using CourseCatalog.Domain.ValueObjects;
+global using FluentValidation;
 global using MediatR;
