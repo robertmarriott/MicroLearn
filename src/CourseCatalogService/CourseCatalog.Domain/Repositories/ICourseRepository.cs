581557620a2850d92cd7324fa6fee9e70df5e9b7
Robert Marriott <13823574+robertmarriott@users.noreply.github.com>
2024-09-09 22:31:23 -0700

    Simplify repository methods

--- src/CourseCatalogService/CourseCatalog.Domain/Repositories/ICourseRepository.cs
+++ src/CourseCatalogService/CourseCatalog.Domain/Repositories/ICourseRepository.cs
@@ -2,9 +2,9 @@
 
 public interface ICourseRepository
 {
-    Task<IReadOnlyCollection<Course>> GetAllAsync(int pageNumber, int pageSize);
+    Task<IEnumerable<Course>> GetAllAsync();
+    Task<IEnumerable<Course>> GetAllOpenForEnrollmentAsync();
     Task<Course?> GetByIdAsync(CourseId id);
     Task AddAsync(Course course);
-    Task UpdateAsync(Course course);
-    Task DeleteAsync(CourseId id);
+    Task DeleteAsync(Course course);
 }
