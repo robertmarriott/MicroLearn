b7276af466398016f825ac10e87277aa72b7cfee
Robert Marriott <13823574+robertmarriott@users.noreply.github.com>
2024-09-29 17:39:53 -0700

    Remove unused events

--- src/CourseCatalogService/CourseCatalog.Domain/Courses/Events/CourseSkillLevelChangedEvent.cs
+++
@@ -1,9 +0,0 @@
-﻿using CourseCatalog.Domain.Common.Base;
-using CourseCatalog.Domain.Courses.Enums;
-using CourseCatalog.Domain.Courses.ValueObjects;
-
-namespace CourseCatalog.Domain.Courses.Events;
-
-public record class CourseSkillLevelChangedEvent(
-    CourseId CourseId,
-    SkillLevel NewSkillLevel) : IDomainEvent;
--- src/CourseCatalogService/CourseCatalog.Domain/Courses/Events/CourseTitleChangedEvent.cs
+++
@@ -1,8 +0,0 @@
-﻿using CourseCatalog.Domain.Common.Base;
-using CourseCatalog.Domain.Courses.ValueObjects;
-
-namespace CourseCatalog.Domain.Courses.Events;
-
-public record class CourseTitleChangedEvent(
-    CourseId CourseId,
-    string NewTitle) : IDomainEvent;
